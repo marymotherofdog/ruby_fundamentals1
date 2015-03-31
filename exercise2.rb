518270bc860d1c590ebf319c1e67bb1b9dbad85a
Mary Viets <mary.evelyn.viets@gmail.com>
2015-03-31 15:16:09 -0400

    added sting interpolation

--- exercise2.rb
+++ exercise2.rb
@@ -1,4 +1,9 @@
 #tip = 55 * 0.2 #calulates 20% tip for $55
 #puts tip # puts tip 
 
-puts "Hello" + 5.to_s
+#puts "Hello" + 5.to_s # changed integer to a string to add
+
+result = 45628 * 7839
+puts "The answer is #{result}."
+
+
