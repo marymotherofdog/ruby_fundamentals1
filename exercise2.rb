a60f30b682ee90769905a247d16400d0cea7bf75
Mary Viets <mary.evelyn.viets@gmail.com>
2015-03-31 15:33:35 -0400

    added the boolean section

--- exercise2.rb
+++ exercise2.rb
@@ -3,7 +3,9 @@
 
 #puts "Hello" + 5.to_s # changed integer to a string to add
 
-result = 45628 * 7839
-puts "The answer is #{result}."
+#result = 45628 * 7839
+#puts "The answer is #{result}." # puts a string with the result
 
+puts (true && false)||(false&&true)||(false&&false)
+# puts the boolean value of the expression
 
